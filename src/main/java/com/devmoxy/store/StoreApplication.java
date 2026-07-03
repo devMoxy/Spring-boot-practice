fad01059dc887ee28090cd521a1ceec600f87455
devMoxy <adelowoenoch1@gmail.com>
2026-07-03 20:34:22 +0100

    sql set up

--- src/main/java/com/devmoxy/store/StoreApplication.java
+++ src/main/java/com/devmoxy/store/StoreApplication.java
@@ -1,12 +1,7 @@
 package com.devmoxy.store;
-
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Scanner;
@@ -17,9 +12,6 @@ public class StoreApplication {
 
     public static void main(String[] args) {
         ConfigurableApplicationContext context   = SpringApplication.run(StoreApplication.class, args);
-        var userService = context.getBean(UserService.class);
-        userService.Register(new User (1, "devMoxy", "devmoxy@gmail.com", "devMoxy123"));
-
     }
 
 //    @Controller
