6834de672e23e9df7100e760211b3218fb641a00
devMoxy <adelowoenoch1@gmail.com>
2026-07-04 22:41:17 +0100

    flyway migration

--- src/main/java/com/devmoxy/store/StoreApplication.java
+++ src/main/java/com/devmoxy/store/StoreApplication.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Scanner;
 
 @SpringBootApplication
-@RestController
 public class StoreApplication {
 
     public static void main(String[] args) {
